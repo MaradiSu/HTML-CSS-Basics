601a2737cf7e548032c219240a46733449d0654c
MaradiSu <p.suresh@campusuvce.in>
2020-10-09 18:35:44 +0530

    JS Bsics added

---
+++ Day 1/js/content.js
@@ -0,0 +1,29 @@
+var eleId ="H1"
+var eleId2 ="H2"
+alert('Hi');
+console.clear();
+console.log('Log enabled');
+console.error('You made a mistake');
+console.assert(eleId,"ELement is there with ID 'H1'")
+console.assert(eleId2,"ELement is there with ID 'H2'")
+
+for(i=0; i<5; i++)
+{
+    console.count();
+}
+console.warn("This is a warning");
+
+
+
+
+var today = new Date();
+var TimeNow= today.getHours();
+
+if(TimeNow>=18)
+alert('Good Evening');
+else if(TimeNow>=12)
+alert('Good Afternoon');
+else if(TimeNow>=0)
+alert('Good Morning');
+else
+alert('Welcome');
