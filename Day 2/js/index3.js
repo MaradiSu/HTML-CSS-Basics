0152d0d1c874266ee50f0ff63f434bc700d5fbf0
MaradiSu <p.suresh@campusuvce.in>
2020-10-12 19:00:35 +0530

    Day 3 commit

---
+++ Day 2/js/index3.js
@@ -0,0 +1,19 @@
+console.log(this === window);
+
+a = 20.15;
+
+console.log(this.a);
+
+
+b= "Suresh" ;
+
+
+console.log(window.b)//suresh
+
+
+console.log(this.b)//suresh
+
+
+
+/* Expressions with respect to functions */
+
